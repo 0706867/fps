30f5975dcc828bffe50da3e47f1c4170c382ff75
0706867 <87291071+0706867@users.noreply.github.com>
2021-07-22 12:23:21 +1000

    init

---
+++ Player.gd
@@ -0,0 +1,160 @@
+extends KinematicBody
+
+const GRAVITY = -24.8
+var vel = Vector3()
+const MAX_SPEED = 20
+const JUMP_SPEED = 18
+const ACCEL = 4.5
+const MAX_SPRINT_SPEED = 70
+const SPRINT_ACCEL = 18
+var is_sprinting = false
+
+var flashlight
+
+var dir = Vector3()
+
+const DEACCEL= 16
+const MAX_SLOPE_ANGLE = 40
+
+var camera
+var rotation_helper
+
+var MOUSE_SENSITIVITY = 0.1
+# Place before _ready
+var animation_manager
+
+var current_weapon_name = "UNARMED"
+var weapons = {"UNARMED":null, "KNIFE":null, "PISTOL":null, "RIFLE":null}
+const WEAPON_NUMBER_TO_NAME = {0:"UNARMED", 1:"KNIFE", 2:"PISTOL", 3:"RIFLE"}
+const WEAPON_NAME_TO_NUMBER = {"UNARMED":0, "KNIFE":1, "PISTOL":2, "RIFLE":3}
+var changing_weapon = false
+var changing_weapon_name = "UNARMED"
+
+var health = 100
+
+var UI_status_label
+
+func _ready():
+	camera = $Rotation_Helper/Camera
+	rotation_helper = $Rotation_Helper
+
+	animation_manager = $Rotation_Helper/Model/Animation_Player
+	animation_manager.callback_function = funcref(self, "fire_bullet")
+
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	weapons["KNIFE"] = $Rotation_Helper/Gun_Fire_Points/Knife_Point
+	weapons["PISTOL"] = $Rotation_Helper/Gun_Fire_Points/Pistol_Point
+	weapons["RIFLE"] = $Rotation_Helper/Gun_Fire_Points/Rifle_Point
+
+	var gun_aim_point_pos = $Rotation_Helper/Gun_Aim_Point.global_transform.origin
+
+	for weapon in weapons:
+		var weapon_node = weapons[weapon]
+		if weapon_node != null:
+			weapon_node.player_node = self
+			weapon_node.look_at(gun_aim_point_pos, Vector3(0, 1, 0))
+			weapon_node.rotate_object_local(Vector3(0, 1, 0), deg2rad(180))
+
+	current_weapon_name = "UNARMED"
+	changing_weapon_name = "UNARMED"
+
+	UI_status_label = $HUD/Panel/Gun_label
+	flashlight = $Rotation_Helper/Flashlight
+func _physics_process(delta):
+	process_input(delta)
+	process_movement(delta)
+
+func process_input(delta):
+# ----------------------------------
+# Sprinting
+	if Input.is_action_pressed("movement_sprint"):
+		is_sprinting = true
+	else:
+		is_sprinting = false
+# ----------------------------------
+
+# ----------------------------------
+# Turning the flashlight on/off
+	if Input.is_action_just_pressed("flashlight"):
+		if flashlight.is_visible_in_tree():
+			flashlight.hide()
+		else:
+			flashlight.show()
+# ----------------------------------
+	# ----------------------------------
+	# Walking
+	dir = Vector3()
+	var cam_xform = camera.get_global_transform()
+
+	var input_movement_vector = Vector2()
+
+	if Input.is_action_pressed("movement_forward"):
+		input_movement_vector.y += 1
+	if Input.is_action_pressed("movement_backward"):
+		input_movement_vector.y -= 1
+	if Input.is_action_pressed("movement_left"):
+		input_movement_vector.x -= 1
+	if Input.is_action_pressed("movement_right"):
+		input_movement_vector.x += 1
+
+	input_movement_vector = input_movement_vector.normalized()
+
+	# Basis vectors are already normalized.
+	dir += -cam_xform.basis.z * input_movement_vector.y
+	dir += cam_xform.basis.x * input_movement_vector.x
+	# ----------------------------------
+
+	# ----------------------------------
+	# Jumping
+	if is_on_floor():
+		if Input.is_action_just_pressed("movement_jump"):
+			vel.y = JUMP_SPEED
+	# ----------------------------------
+
+	# ----------------------------------
+	# Capturing/Freeing the cursor
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	# ----------------------------------
+
+func process_movement(delta):
+	dir.y = 0
+	dir = dir.normalized()
+
+	vel.y += delta * GRAVITY
+
+	var hvel = vel
+	hvel.y = 0
+
+	var target = dir
+	if is_sprinting:
+		target *= MAX_SPRINT_SPEED
+	else:
+		target *= MAX_SPEED
+
+	var accel
+	if dir.dot(hvel) > 0:
+		if is_sprinting:
+			accel = SPRINT_ACCEL
+		else:
+			accel = ACCEL
+	else:
+		accel = DEACCEL
+
+	hvel = hvel.linear_interpolate(target, accel * delta)
+	vel.x = hvel.x
+	vel.z = hvel.z
+	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
+		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
+
+		var camera_rot = rotation_helper.rotation_degrees
+		camera_rot.x = clamp(camera_rot.x, -70, 70)
+		rotation_helper.rotation_degrees = camera_rot
