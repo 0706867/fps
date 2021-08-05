6514a6974858c801f41a0968ee2275de6c9531d5
0706867 <87291071+0706867@users.noreply.github.com>
2021-08-05 13:00:28 +1000

    comments
    ammopickup.gd done a bit

--- Ammo_Pickup.gd
+++ Ammo_Pickup.gd
@@ -1,31 +1,31 @@
-extends Spatial
+extends Spatial																			#can only be used on a spatial node 
 
-export (int, "full size", "small") var kit_size = 0 setget kit_size_change
+export (int, "full size", "small") var kit_size = 0 setget kit_size_change				#export fucntion lets the developer change the size from the inspector
 
 # 0 = full size pickup, 1 = small pickup
-const AMMO_AMOUNTS = [4, 1]
-const RESPAWN_TIME = 20
-var respawn_timer = 0
-var is_ready = false
-const GRENADE_AMOUNTS = [2, 0]
+const AMMO_AMOUNTS = [4, 1]																	#ammo amount first entry[0] = 4 which gives 4 magazines and second entry[1] = 1 gives 1 magazine
+const RESPAWN_TIME = 20																		#respawn timer for the ammo pickup
+var respawn_timer = 0																		#time on the respawn timer
+var is_ready = false																		#can it be picked up
+const GRENADE_AMOUNTS = [2, 0]																#how many grenades get filled up
 
 func _ready():
 
-	$Holder/Ammo_Pickup_Trigger.connect("body_entered", self, "trigger_body_entered")
+	$Holder/Ammo_Pickup_Trigger.connect("body_entered", self, "trigger_body_entered")		#connects the node to the functions, when something collies it enables the trigger body function
 
-	is_ready = true
+	is_ready = true																	# is able to be picked up
 
-	kit_size_change_values(0, false)
-	kit_size_change_values(1, false)
+	kit_size_change_values(0, false)												#change value to 0(first entry)? no
+	kit_size_change_values(1, false)												#change value to 1? (second entry) no
 
-	kit_size_change_values(kit_size, true)
+	kit_size_change_values(kit_size, true)											#change the value to the size provided in the inspector? yes
 
 
 func _physics_process(delta):
-	if respawn_timer > 0:
+	if respawn_timer > 0:															#if timer is higher than 0, remove delta from the timer
 		respawn_timer -= delta
 
-		if respawn_timer <= 0:
+		if respawn_timer <= 0:														#if the time lower than 0, spawn the turret
 			kit_size_change_values(kit_size, true)
 
 
