cf3c2bf857ac0009f1a9fb347f0f1adce99a9f03
0706867 <87291071+0706867@users.noreply.github.com>
2021-08-22 22:30:01 +1000

    commented turretbodies.gd

--- TurretBodies.gd
+++ TurretBodies.gd
@@ -1,10 +1,10 @@
 extends StaticBody
 
-export (NodePath) var path_to_turret_root
+export (NodePath) var path_to_turret_root											#gets the path for turret body
 
 func _ready():
 	pass
 
-func bullet_hit(damage, bullet_hit_pos):
+func bullet_hit(damage, bullet_hit_pos):											#if the turret gets hit, and there is a path to the turret body, do damage.
 	if path_to_turret_root != null:
 		get_node(path_to_turret_root).bullet_hit(damage, bullet_hit_pos)
