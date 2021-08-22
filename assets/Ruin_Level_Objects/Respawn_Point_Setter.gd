5c913fd0c8b6ece809e395d48cd3165b86db24ba
0706867 <87291071+0706867@users.noreply.github.com>
2021-08-22 20:49:28 +1000

    commented respawn point setter script

--- assets/Ruin_Level_Objects/Respawn_Point_Setter.gd
+++ assets/Ruin_Level_Objects/Respawn_Point_Setter.gd
@@ -1,5 +1,5 @@
 extends Spatial
 
-func _ready():
+func _ready():																		#get the node this script is attached to and set the respawn points in the global script as the current nodes children
 	var globals = get_node("/root/Globals")
 	globals.respawn_points = get_children()
