adfb4199b9c9a1fb1b5ee8641a5938de16c3992c
0706867 <87291071+0706867@users.noreply.github.com>
2021-08-22 20:50:45 +1000

    commented rigid body test.gd

--- RigidBody_hit_test.gd
+++ RigidBody_hit_test.gd
@@ -1,10 +1,10 @@
 extends RigidBody
 
-const BASE_BULLET_BOOST = 9;
+const BASE_BULLET_BOOST = 9;														#force applied when shot
 
 func _ready():
 	pass
 
-func bullet_hit(damage, bullet_global_trans):
+func bullet_hit(damage, bullet_global_trans):										#get the direction fo the bullet and apply the BASE BULLET BOOST to it in the direction
 	var direction_vect = bullet_global_trans.basis.z.normalized() * BASE_BULLET_BOOST
 	apply_impulse((bullet_global_trans.origin - global_transform.origin).normalized(), direction_vect * damage)
