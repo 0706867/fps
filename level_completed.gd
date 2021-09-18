f56d400c78ddf818ffc4d2ec8531485f5581268e
0706867 <87291071+0706867@users.noreply.github.com>
2021-09-18 19:25:58 +1000

    changed planes to cubes
    helps with texturing, changed all walls, roof and floor planes to cubes

--- level_completed.gd
+++ level_completed.gd
@@ -6,11 +6,11 @@ export (String, FILE) var main
 func _ready():
 #connnects the 2 buttons to function below
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	$end/replay.connect("pressed", self, "finish", ["replay"])
-	$end/main_menu.connect("pressed", self, "finish", ["main_menu"])
+	$end/replay.connect("pressed", self, "finish", ["replay"])			#replay button to the main_level scene
+	$end/main_menu.connect("pressed", self, "finish", ["main_menu"])	#main_menu button to the main_menu scene
 	
 func finish (button_name):
-	if button_name == "replay":
+	if button_name == "replay":											#copied from the main_menu.gd script, names chanegd to fit this script
 		get_node("/root/Globals").load_new_scene(replay)
 	if button_name == "main_menu":
 		get_node("/root/Globals").load_new_scene(main)
