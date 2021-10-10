a64725c4ea0e9cabe150db661044b80f16906308
0706867 <87291071+0706867@users.noreply.github.com>
2021-10-11 01:11:54 +1100

    bears now move
    currently the only go forward and up, but with fixes they should go forward and turn around

--- bear.gd
+++ bear.gd
@@ -1,17 +1,28 @@
-extends RigidBody
+extends KinematicBody
 
+var speed = 200
 var current_health = 10
 var hit = false
 var collider
-
+var target
+var walking = Vector3(20, 0,10)
 func _ready():
 	Globals.enemy_amount +=1														#adds to total enemy amount in global script
 	current_health = 20																#bear health
 	collider = $CollisionShape
 
+func _process(delta):
+	move(delta)
+	
+func move(delta):
+	var direction = (get_parent().transform.origin - transform.origin).normalized()
+	move_and_slide(direction * speed * delta, Vector3.UP)
+
+
 func bullet_hit(damage, bullet_pos):															#allows the target to take damage
 	hit = true																		#enemy is hit
-	if hit == true:																	#if enemy is hit, lower the enemy amount
+	if hit:																	#if enemy is hit, lower the enemy amount
 		Globals.enemy_amount -=1
 		collider.disabled = true
 		visible = false
+
