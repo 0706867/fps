fcac42d9742b04302f8e6daa103ae56c9d8ac353
0706867 <87291071+0706867@users.noreply.github.com>
2021-10-12 14:01:27 +1100

    bears can move
    Bears now move forwards and backwards, based on a timer, they can not currently turn around which is what im working on now.

--- bear.gd
+++ bear.gd
@@ -1,22 +1,41 @@
 extends KinematicBody
 
-var speed = 200
+var speed = 15
 var current_health = 10
 var hit = false
 var collider
 var target
-var walking = Vector3(20, 0,10)
+var timer 
+var turn = false
+
 func _ready():
 	Globals.enemy_amount +=1														#adds to total enemy amount in global script
 	current_health = 20																#bear health
 	collider = $CollisionShape
+	timer = 0
+	#location = Vector3(get_parent().transform.origin)
 
 func _process(delta):
 	move(delta)
-	
+	timer += 0.015 
+
+
 func move(delta):
-	var direction = (get_parent().transform.origin - transform.origin).normalized()
-	move_and_slide(direction * speed * delta, Vector3.UP)
+#	var direction = (get_parent().transform.origin).normalized()
+	var direction = Vector3(-2,0,0) 
+	var location_x = Vector3(transform.origin.x + (1*speed), 0,0)
+	move_and_slide(location_x, Vector3())
+	
+	if timer >= 2:
+		turn = true
+		if turn:
+			if rotation.y == 0 :
+				rotation.y = 180
+			if rotation.y == 180 :
+				rotation.y = 0
+			speed = -speed
+			timer = 0
+			turn = false
 
 
 func bullet_hit(damage, bullet_pos):															#allows the target to take damage
