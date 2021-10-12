b8ce5e78310f237fee6bc775eb1eccef4257301b
0706867 <87291071+0706867@users.noreply.github.com>
2021-10-12 23:24:27 +1100

    fixed Ai
    works better, auto rotate feature works better with more accuracy, commented most of the bear file as well

--- bear.gd
+++ bear.gd
@@ -11,6 +11,14 @@ func _ready():
 	collider = $CollisionShape														#connect bears collisionshape to the variable
 	timer = 0																		#reset the timer
 	print(rad2deg((get_parent().rotation.y)))
+	if rad2deg((get_parent().rotation.y)) >= 315 and rad2deg((get_parent().rotation.y)) <= 360 or rad2deg((get_parent().rotation.y)) <= 45 and rad2deg((get_parent().rotation.y)) >= 0 or rad2deg((get_parent().rotation.y)) >= -45 and rad2deg((get_parent().rotation.y)) <= 0 or rad2deg((get_parent().rotation.y)) <= -315 and rad2deg((get_parent().rotation.y)) >= 0:
+		get_parent().rotation.y = deg2rad(1)											#set face to 0 degrees or facing right
+	elif rad2deg((get_parent().rotation.y)) >= 135 and rad2deg((get_parent().rotation.y)) <= 225 or rad2deg((get_parent().rotation.y)) <= -135 and rad2deg((get_parent().rotation.y)) >= -225:
+		get_parent().rotation.y = deg2rad(180)											#set face to 180 degrees or facing left
+	elif rad2deg((get_parent().rotation.y)) >= 45 and rad2deg((get_parent().rotation.y)) <= 135 or rad2deg((get_parent().rotation.y)) <= -45 and rad2deg((get_parent().rotation.y)) >= -135:
+		get_parent().rotation.y = deg2rad(90)											#set face to 90 degrees or facing UP
+	elif rad2deg((get_parent().rotation.y)) >= 225 and rad2deg((get_parent().rotation.y)) <= 315 or rad2deg((get_parent().rotation.y)) <= -225 and rad2deg((get_parent().rotation.y)) >= -315:
+		get_parent().rotation.y = deg2rad(270)											#set face to 270 degrees or facing down
 
 func _process(delta):
 	move(delta)
@@ -21,18 +29,14 @@ func move(delta):
 	var location_z = Vector3(0,0,transform.origin.z + (1*speed))					#gets the local z position and add speed to it
 	var location_x = Vector3(transform.origin.x + (1*speed),0,0)					#gets the local x position and add speed to it
 
-	if rad2deg((get_parent().rotation.y)) >= 315 and rad2deg((get_parent().rotation.y)) <= 45 or rad2deg((get_parent().rotation.y)) >= -45 and rad2deg((get_parent().rotation.y)) <= -315:
-		move_and_slide(-location_z, Vector3())											#use the z position for moving
-		get_parent().rotation.y = deg2rad(0)
-	elif rad2deg((get_parent().rotation.y)) >= 135 and rad2deg((get_parent().rotation.y)) <= 225 or rad2deg((get_parent().rotation.y)) <= -135 and rad2deg((get_parent().rotation.y)) >= -225:
-		move_and_slide(location_z, Vector3())											#use the x position for moving
-		get_parent().rotation.y = deg2rad(180)
+	if rad2deg((get_parent().rotation.y)) >= 135 and rad2deg((get_parent().rotation.y)) <= 225 or rad2deg((get_parent().rotation.y)) <= -135 and rad2deg((get_parent().rotation.y)) >= -225:
+		move_and_slide(location_z, Vector3())											#move left
+	elif rad2deg((get_parent().rotation.y)) >= 315 and rad2deg((get_parent().rotation.y)) <= 360 or rad2deg((get_parent().rotation.y)) <= 45 and rad2deg((get_parent().rotation.y)) >= 0 or rad2deg((get_parent().rotation.y)) >= -45 and rad2deg((get_parent().rotation.y)) <= 0 or rad2deg((get_parent().rotation.y)) <= -315 and rad2deg((get_parent().rotation.y)) >= 0:
+		move_and_slide(-location_z, Vector3())											#move right
 	elif rad2deg((get_parent().rotation.y)) >= 45 and rad2deg((get_parent().rotation.y)) <= 135 or rad2deg((get_parent().rotation.y)) <= -45 and rad2deg((get_parent().rotation.y)) >= -135:
-		move_and_slide(-location_x, Vector3())											#use the x position for moving
-		get_parent().rotation.y = deg2rad(90)
+		move_and_slide(-location_x, Vector3())											#move up
 	elif rad2deg((get_parent().rotation.y)) >= 225 and rad2deg((get_parent().rotation.y)) <= 315 or rad2deg((get_parent().rotation.y)) <= -225 and rad2deg((get_parent().rotation.y)) >= -315:
-		move_and_slide(location_x, Vector3())											#use the x position for moving
-		get_parent().rotation.y = deg2rad(270)
+		move_and_slide(location_x, Vector3())											#move down
 	if timer >= 2:																	#if timer hits 2 secs
 		turn = true																	#make turn true
 		if turn:																	#if turn in true
